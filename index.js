44a7df13dab7a530f8597d7083f3a7dc78edb492
Šárka Klauberová <sarkaa.novakova@seznam.cz>
2023-09-30 17:14:38 +0200

    Done

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
+const handleDigitClick = (event) => {
+  const displayElm = document.querySelector('.display');
+  const obsah = event.target.textContent;
+  if (displayElm.textContent === '0') {
+    return (displayElm.textContent = obsah);
+  } else if (displayElm.textContent.length < 9) {
+    displayElm.textContent += obsah;
+  }
+};
 
+document.querySelector('#btn-0').addEventListener('click', handleDigitClick);
+document.querySelector('#btn-1').addEventListener('click', handleDigitClick);
+document.querySelector('#btn-2').addEventListener('click', handleDigitClick);
+document.querySelector('#btn-3').addEventListener('click', handleDigitClick);
+document.querySelector('#btn-4').addEventListener('click', handleDigitClick);
+document.querySelector('#btn-5').addEventListener('click', handleDigitClick);
+document.querySelector('#btn-6').addEventListener('click', handleDigitClick);
+document.querySelector('#btn-7').addEventListener('click', handleDigitClick);
+document.querySelector('#btn-8').addEventListener('click', handleDigitClick);
+document.querySelector('#btn-9').addEventListener('click', handleDigitClick);
